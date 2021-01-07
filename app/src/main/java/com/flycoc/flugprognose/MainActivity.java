ff6c5e29523d8dfa8f9949bb6a5be94661418c16
COC <cyrill.coray@students.fhnw.ch>
2021-01-07 20:55:37 +0100

    Slider und dynamisches Laden der Verlässlichkeit einfegügt

--- app/src/main/java/com/flycoc/flugprognose/MainActivity.java
+++ app/src/main/java/com/flycoc/flugprognose/MainActivity.java
@@ -1,11 +1,15 @@
 package com.flycoc.flugprognose;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.flycoc.flugprognose.utils.LoadImageHelper;
+import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imgViewVerlässlichkeit;
     ImageView imgViewFöhn;
+
+    Slider sliderVerlässlichkeit;
 
 
     @Override
@@ -26,7 +32,26 @@ public class MainActivity extends AppCompatActivity {
         imageHelper.loadImage(this,getString(R.string.urlVerlässlichkeit),imgViewVerlässlichkeit);
         imageHelper.loadImage(this,getString(R.string.urlFöhndiagramm),imgViewFöhn);
 
+        sliderVerlässlichkeit = (Slider) findViewById(R.id.sliderVerlässlichkeit);
 
+        sliderVerlässlichkeit.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    String hours = String.valueOf((int)sliderVerlässlichkeit.getValue());
+                    String url = "https://www.wetterzentrale.de/maps/GFSSPAGEU18_" + hours + "_1.png";
+                    Log.i("+++++++++++++","Load image from URL: "+url);
+
+                    reloadImage(url,imgViewVerlässlichkeit);
+                }
+                return false;
+            }
+        });
+    }
+
+    private void reloadImage(String url, ImageView imgView) {
+        imageHelper.loadImage(this,url,imgView);
     }
 
 }
