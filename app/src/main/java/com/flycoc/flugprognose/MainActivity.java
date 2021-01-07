15012c64503ebdd169ace0825c570d5cd3751f7b
COC <cyrill.coray@students.fhnw.ch>
2021-01-07 21:57:18 +0100

    Allgemeine Lage hinzugefügt

--- app/src/main/java/com/flycoc/flugprognose/MainActivity.java
+++ app/src/main/java/com/flycoc/flugprognose/MainActivity.java
@@ -5,19 +5,27 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.flycoc.flugprognose.utils.LoadImageHelper;
 import com.google.android.material.slider.Slider;
+import com.ortiz.touchview.TouchImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     LoadImageHelper imageHelper = new LoadImageHelper();
 
-    ImageView imgViewVerlässlichkeit;
+    TouchImageView imgViewVerlässlichkeit;
     ImageView imgViewFöhn;
+
+    TextView textAllgLageDatum;
+    TouchImageView imgViewAllgLage;
 
     Slider sliderVerlässlichkeit;
 
@@ -27,13 +35,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgViewVerlässlichkeit = (ImageView) findViewById(R.id.imgViewVerlässlichkeit);
-        imgViewFöhn = (ImageView) findViewById(R.id.imgViewFöhn);
+        imgViewAllgLage = findViewById(R.id.imgViewAllgLage);
 
+        imgViewVerlässlichkeit = findViewById(R.id.imgViewVerlässlichkeit);
         imageHelper.loadImage(this,getString(R.string.urlVerlässlichkeit),imgViewVerlässlichkeit);
+
+        imgViewFöhn = findViewById(R.id.imgViewFöhn);
         imageHelper.loadImage(this,getString(R.string.urlFöhndiagramm),imgViewFöhn);
 
-        sliderVerlässlichkeit = (Slider) findViewById(R.id.sliderVerlässlichkeit);
+        textAllgLageDatum = findViewById(R.id.textAllgLageDatum);
+        String dateText = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        textAllgLageDatum.setText(dateText);
+        imgViewAllgLage = (TouchImageView) findViewById(R.id.imgViewAllgLage);
+        String date = new SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(new Date());
+        String urlAllgLage = "https://media.meteonews.net/isobaren/EURNAt_700x450_c1/isobaren_"+date+"_1200.png";
+        imageHelper.loadImage(this, urlAllgLage, imgViewAllgLage);
+
+
+
+        sliderVerlässlichkeit = findViewById(R.id.sliderVerlässlichkeit);
 
         sliderVerlässlichkeit.setOnTouchListener(new View.OnTouchListener() {
             @Override
